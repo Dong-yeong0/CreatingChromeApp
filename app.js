0797e887aad7e0ca284a3451aa2d8cbf7ccceb8a
Dong-yeong0 <wjdehddud77@naver.com>
2022-01-12 22:54:23 +0900

    바닐라 JS로 크롬 앱 만들기 31% -> 47% addEventListener 이벤트 종류 (click, resize, mouseenter, leave, offline, online) 제일 신기했던건 WIFI 연결 상태도 이벤트 걸 수 있구나.. querySelector()를 알고나니 getElementById 랑 같이 써봐야겠고, 점점 알 수록 재밌다

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+// 2022 01 11
 /**
  *  Variable
  *  var let const
@@ -6,12 +7,12 @@
  *  const : 상수로서 절대로 변하지 않는 값을 가진다. (업데이트 X, 재선언 X)
  */
 // 재선언, 업데이트 할 수 없는 불변의 값
-const a = 5;
-const b = 5;
+// const a = 5;
+// const b = 5;
 
-let myName = "Dongyeong";
-// let = 재선언 가능 (업데이트 허용)
-myName = "Anonymous";
+// let myName = "Dongyeong";
+// // let = 재선언 가능 (업데이트 허용)
+// myName = "Anonymous";
 
 /**
  * undefined, true, false, null
@@ -25,17 +26,17 @@ myName = "Anonymous";
  * Array
  * index가 있어 정보를 찾을 때 유용
  */
-const daysOfWeek = ["mon", "tue", "wed", "thu", "fri", "sat"];
-delete daysOfWeek[1];   // tue 삭제 (result = empty)
-console.log(daysOfWeek);
+// const daysOfWeek = ["mon", "tue", "wed", "thu", "fri", "sat"];
+// delete daysOfWeek[1];   // tue 삭제 (result = empty)
+// console.log(daysOfWeek);
 
-// 지우고 다시 넣으면 맨 뒤에 push 된다.
-daysOfWeek.push("tru");
-console.log(daysOfWeek);
+// // 지우고 다시 넣으면 맨 뒤에 push 된다.
+// daysOfWeek.push("tru");
+// console.log(daysOfWeek);
 
-// empty 자리에 값 넣기 (push는 안먹히네)
-daysOfWeek[1] = "tue";
-console.log(daysOfWeek);
+// // empty 자리에 값 넣기 (push는 안먹히네)
+// daysOfWeek[1] = "tue";
+// console.log(daysOfWeek);
 
 /**
  * List
@@ -62,7 +63,7 @@ sayHello("anonymous");
 */
 // List, Function응용
 
-const player = {
+/*const player = {
     name : "Dongyeong",
     sayHello : function() {
         console.log("Hello");
@@ -78,4 +79,81 @@ function calculaterEuAge(ageOfForeigner) {
 }
 const EuAge = calculaterEuAge(age);
 
-console.log(EuAge);
+console.log(EuAge);*/
+
+// 2022 01 12
+// prompt = 사용자에게 창을 띄울 수 있게 해줌 (message(String), defualt)
+// NaN = Not a Number
+/*const age = prompt("How old are you?");
+// 입력 되기 전 까지 페이지는 javascript를 일시정지를 시킨다. (CSS 적용 X)
+console.log(age);
+// 입력값 parsing
+console.log(typeof age);                // input : 12 / output String
+console.log(parseInt(age));
+console.log(typeof parseInt(age));      // input : 12 / output Number
+
+
+// isNaN() : return 값은 boolean (true, false)
+console.log(isNaN(age));
+*/
+/*
+const title = document.querySelector(".hello h1");
+console.log(title);
+
+const allTitle = document.querySelectorAll(".hello h1");
+
+console.log(allTitle)
+*/
+// 이 app.js가 있기에 javascript를 통해 HTMl의 내용을 가지고 올 수 있다.
+// querySelector로 CSS Selector 전달을 할 수 있다네
+
+/**
+ * firstTitle.innerHTML = "first-child"; 이게 없으면 object 타입으로 표시한 element를 보여줌
+ * 여기서 on~~ 뭐 나오는데 이렇게 많았나?
+ * 
+ */ 
+// 와 나 맨날 $(".hello").style("color", "red"); 이런 방식으로 바꿨는데 이렇게도 되네
+//firstTitle.style.color = "red";
+
+// 드디어 event (종류가 엄청나다)
+const firstTitle = document.querySelector("div.hello:first-child h1");
+function titleColorChange() {
+    firstTitle.style.color = "blue";
+}
+
+function handleMouseEnter() {
+    firstTitle.innerHTML = "Mouse is Here";
+}
+
+function handleMouseLeave() {
+    firstTitle.innerHTML = "Mouse is Gone";
+}
+
+function handleWindowResize() {
+    document.body.style.backgroundColor = "tomato";
+}
+
+function handleWindowCopy() {
+    alert("Copyer!");
+}
+
+function handWindowOffline() {
+    alert("SOS no WIFI");
+}
+
+function handWindowOnline() {
+    alert("GOOD");
+}
+// 여러종류 맛보기 (여기서 함수를 실행시킬 수 있게 () 를 포함 안시켜도 Javascript가 Execute 시켜줌)
+//firstTitle.addEventListener("click", titleColorChange);
+// 이런 방식도 있지만 더욱 선호하는건 위의 방식 왜? removeEventListener로 제거할 수 있기 때문.
+firstTitle.onclick = titleColorChange;
+firstTitle.addEventListener("mouseenter", handleMouseEnter);
+firstTitle.addEventListener("mouseleave", handleMouseLeave);
+
+window.addEventListener("resize", handleWindowResize);
+window.addEventListener("copy", handleWindowCopy)
+// document.body, head, title는 콘솔에서 정상 출력이 되지만 div같은 element들은 querySelector나 getElementById 등으로 가져와야된다.
+// 무슨 WIFI 연결상태도 event 걸 수 있네
+window.addEventListener("offline", handWindowOffline);  // 연결 끊겼을 때
+window.addEventListener("online", handWindowOnline);    // 연결 되었을 때

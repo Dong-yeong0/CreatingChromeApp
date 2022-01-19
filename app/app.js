c59160ad7f854a0af02863145d6a7b49f2ace1d6
Dong-yeong0 <wjdehddud77@naver.com>
2022-01-19 23:12:14 +0900

    prevent 수정

--- app/app.js
+++ app/app.js
@@ -1,11 +1,20 @@
 const loginInput = document.querySelector("#login-form input");
 const loginForm = document.querySelector("#login-form");
 
+const link = document.querySelector("a");
+
 // eventListener의 함수 첫번째 argument는 항상 submit의 정보가 된다
 function onLoginSubmit(event) {
     // .preventDefault - 브라우저의 기본 동작을 막아줌
-    event.preventDefault();
+    
     console.log(event);
 }
+
+function handleLinkClick() {
+    // alert OK 누르면 a href 로 이동
+    alert("click");
+}
+
 // form submit 새로고침은 기본 동작이고 브라우저가 그러도록 프로그래밍이 되어있다.
 loginForm.addEventListener("submit", onLoginSubmit);
+link.addEventListener("click", handleLinkClick)

cfc4682b082abc5b29817ffcddce5c4dee854f2a
Dong-yeong0 <wjdehddud77@naver.com>
2022-02-01 01:32:07 +0900

    CSS 수정 및 Date 추가

--- js/clock.js
+++ js/clock.js
@@ -1,4 +1,5 @@
-const clock = document.querySelector("#clock");
+const time = document.querySelector("#timePrint");
+const date = document.querySelector("#datePrint");
 
 function getClock() {
     const date = new Date();
@@ -6,10 +7,20 @@ function getClock() {
     const hours = String(date.getHours()).padStart(2, "0");
     const minutes = String(date.getMinutes()).padStart(2, "0");
     const seconds = String(date.getSeconds()).padStart(2, "0");
-    clock.innerText = `${hours}:${minutes}:${seconds}`
+
+    const weeks = ['SUN','MON','TUE','WEB','THU','FRI','SAT'];
+    const years = date.getFullYear();
+    const month = date.getMonth() + 1;
+    const days = date.getDay();
+    const week = weeks[date.getDay()];
+
+    timePrint.innerText = `${hours}:${minutes}:${seconds}`;
+    datePrint.innerText = `${years}.${month}.${days}.${week}`;
 }   
 // setInterval(사용할려는 함수 이름, 호출시간간격); -- 시간 간격 함수 호출
 //setInterval(sayHello, 5000);
+
+
 
 // web 로딩시 바로 호출 -> 1초 후 함수 살행
 getClock();
